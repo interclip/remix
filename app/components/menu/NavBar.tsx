4bb5d65526a06ac087aa0e0e978c71a4a6acf4f5
Filip Troníček <filip.tronicek@seznam.cz>
2024-01-14 18:44:54 +0100

    Images

--- app/components/menu/NavBar.tsx
+++ app/components/menu/NavBar.tsx
@@ -1,9 +1,13 @@
 import { MenuItem } from "./MenuItem";
+import interclipLogo from "~/assets/logo.png";
 
 export const NavBar = () => {
     return (
         <nav>
             <ul className="flex h-14 flex-row items-center justify-start gap-2 bg-stone-900 px-8 text-white">
+                <MenuItem to="/">
+                    <img src={interclipLogo} alt="iclip logo" className="mr-4 size-10" />
+                </MenuItem>
                 <MenuItem to="/">Clip a link</MenuItem>
                 <MenuItem to="/receive">Receive a clip</MenuItem>
             </ul>
--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ export default function App() {
     return (
         <html lang="en">
             <head>
+                <title>Interclip</title>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <Meta />
